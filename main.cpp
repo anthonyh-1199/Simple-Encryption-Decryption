78662ee6ddc21bf53dca0efe4622714dac03d934
Anthony Howell <54863044+anthonyh-1199@users.noreply.github.com>
2021-03-02 15:06:57 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// Simple C++ program that allows the user to encrypt / decrypt a message
+  
+// Header file for input output functions 
+#include<iostream>  
+  
+using namespace std; 
+  
+int main() 
+{ 
+	//Instantiate variables
+	string inputMessage, outputMessage;
+	int cipherKey;
+	
+    //User inputs their message
+    cout << "Please enter your message: "; 
+    
+    cin >> inputMessage;
+    
+    cout << inputMessage;
+    
+    
+      
+    return 0; 
+} 
