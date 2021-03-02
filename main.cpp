9c907cdedfc758b2b930cd13be43a39fef17465a
Anthony Howell <54863044+anthonyh-1199@users.noreply.github.com>
2021-03-02 15:30:39 -0600

    Added basic user inputs & algorithm outline

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
+// Simple Cipher
+// Anthony Howell 
+// March 2021
 // Simple C++ program that allows the user to encrypt / decrypt a message
-  
-// Header file for input output functions 
+
 #include<iostream>  
-  
 using namespace std; 
   
 int main() 
@@ -10,20 +11,43 @@ int main()
 	//Instantiate variables
 	string inputMessage, outputMessage;
 	int cipherKey;
+	char actionType;
 	
     //User inputs their message
-
+    cout << "Please enter your message: "; 
+    
+    cin >> inputMessage;
     
     //User chooses to encrypt or decrypt their message
+    cout << "\nEnter the corresponding letter (e/d) if you wish to encrypt or decrypt: ";
     
+    cin >> actionType;
     
-    //User enters their key
-    
-    
-    //Encryption / decryption algorithm
-    
-    
-    //Print the resulting message
+    //Encryption code
+    if (actionType == 'e' or actionType == 'E'){
+    	//Use specific key or random key?
+    	
+    	//Encrypt the message
+    	
+    	//Print out the encrypted message
+    	
+    	//Print out the key
+	}
+	
+	//Decryption code
+	else if (actionType == 'd' or actionType == 'D'){
+		//Enter the decryption key
+		
+		//Decrypt the message
+		
+		//Print out the decrypted message
+	}
+	
+	//Invalid actionType
+	else {
+		cout << "Invalid input, exiting application...";
+	}
+
     
       
     return 0; 
